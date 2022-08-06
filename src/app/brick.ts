a1be0a397f09891c15bc5dfe7edbeb9891599a9b
Arnau Sanchez <pyarnau@gmail.com>
2022-08-06 23:57:43 +0200

    indent

--- src/app/brick.ts
+++ src/app/brick.ts
@@ -1,9 +1,9 @@
-import { BoxGeometry, Color, Mesh, MeshBasicMaterial } from 'three';
+import { BoxGeometry, Color, Mesh, MeshBasicMaterial } from "three";
 
 export class Brick extends Mesh {
-  constructor(size: number, color: Color) {
-    super();
-    this.geometry = new BoxGeometry(size, size, size);
-    this.material = new MeshBasicMaterial({ color });
-  }
+    constructor(size: number, color: Color) {
+        super();
+        this.geometry = new BoxGeometry(size, size, size);
+        this.material = new MeshBasicMaterial({ color });
+    }
 }
